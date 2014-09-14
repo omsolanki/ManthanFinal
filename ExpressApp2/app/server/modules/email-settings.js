018ea8d89f17a9342dc19fa53d1f05bb7d7d1af2
omsolanki <solanki.om@gmail.com>
2014-09-14 07:20:10 +0530

    with new Index page

---
+++ ExpressApp2/app/server/modules/email-settings.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+	
+	host		: 'smtp.gmail.com',
+	user 		: 'omprakash.solanki@infobeans.com',
+	password 	: '*******',
+	sender		: 'Om Prakash Solanki<omprakash.solanki@infobeans.com>'	
+}
